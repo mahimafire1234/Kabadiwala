8ea4c177fc4613bd2047ce23dc1b8be98b71dfb2
KarkiRajiv <rajivkarky15@gmail.com>
2021-12-14 21:37:31 +0545

    adjusting login design

--- lib/main.dart
+++ lib/main.dart
@@ -25,61 +25,63 @@ class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("app name"),
-        backgroundColor: Colors.blue,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: const [
-          Center(
-            child: Padding(
-              padding: EdgeInsets.only(top: 80.0),
-              child: Text(
-                "Login",
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Center(
+              child: Padding(
+                padding: EdgeInsets.only(top: 80.0),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 20),
-            child: Center(
-              child: TextField(
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    fillColor: Colors.black,
-                    prefixIcon: Icon(
-                      CupertinoIcons.envelope,
-                      color: Color(0xFF000000),
-                    ),
-                    hintText: "Email Address",
-                    contentPadding: EdgeInsets.only(left: 80.0)),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+              child: Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 2.0)),
+                      fillColor: Colors.black,
+                      prefixIcon: Icon(
+                        CupertinoIcons.envelope,
+                        color: Color(0xFF000000),
+                      ),
+                      hintText: "Email Address",
+                      contentPadding: EdgeInsets.only(left: 80.0)),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 25),
-            child: Center(
-              child: TextField(
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    prefixIcon: Icon(
-                      CupertinoIcons.lock,
-                      color: Color(0xFF000000),
-                    ),
-                    suffixIcon: Icon(Icons.visibility_off),
-                    hintText: "Enter Password",
-                    contentPadding: EdgeInsets.only(left: 80.0)),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 25),
+              child: Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 2.0)),
+                      prefixIcon: Icon(
+                        CupertinoIcons.lock,
+                        color: Color(0xFF000000),
+                      ),
+                      suffixIcon: Icon(Icons.visibility_off),
+                      hintText: "Enter Password",
+                      contentPadding: EdgeInsets.only(left: 80.0)),
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
