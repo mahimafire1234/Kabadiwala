4c801e6e95364e2b75dbdb3c0d8b3c1dce634dc6
KarkiRajiv <rajivkarky15@gmail.com>
2021-12-17 17:10:44 +0545

    :coffin: remove dead code

--- lib/Kabadiwalaside/MyprofileKabadiwala.dart
+++ lib/Kabadiwalaside/MyprofileKabadiwala.dart
@@ -1,39 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(MyApp());
-
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: MyProfile()
-    );
-  }
-}
-
-//my profile widget
-
-class MyProfile extends StatefulWidget{
+class MyProfile extends StatefulWidget {
   const MyProfile({Key? key}) : super(key: key);
 
   @override
-  MyProfileState createState()=> MyProfileState();
+  MyProfileState createState() => MyProfileState();
 }
 
-class MyProfileState extends State<MyProfile>{
-
+class MyProfileState extends State<MyProfile> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       appBar: AppBar(
           leading: IconButton(
-            icon: Icon(Icons.menu_rounded), onPressed: () {  },
+            icon: Icon(Icons.menu_rounded),
+            onPressed: () {},
           ),
-          title:Center(child:Text("Kabadiwala")),
+          title: Center(child: Text("Kabadiwala")),
           backgroundColor: Color(0xff0077B6)),
-
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
@@ -42,7 +26,7 @@ class MyProfileState extends State<MyProfile>{
               Center(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children:[
+                  children: [
                     Image(
                       image: AssetImage("assets/images/cycling.png"),
                       width: 200,
@@ -52,91 +36,105 @@ class MyProfileState extends State<MyProfile>{
                 ),
               ),
               Padding(
-                padding: EdgeInsets.all(10),
-                child:Text(
-                "Company name",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 15,
-                ))
-              ),
-              Padding(
                   padding: EdgeInsets.all(10),
-                  child:Text(
-                      "Address name",
+                  child: Text("Company name",
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         fontWeight: FontWeight.normal,
                         fontSize: 15,
-                      ))
-              ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: ElevatedButton(
-              child: Text("Edit profile",style: TextStyle(fontSize: 20),),
-              onPressed: (){},
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Color(0xff0077B6)),
-                fixedSize: MaterialStateProperty.all(Size(240.0,50.0)),
-
-              ),
-            )),
+                      ))),
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text("Address name",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 15,
+                      ))),
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                    child: Text(
+                      "Edit profile",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xff0077B6)),
+                      fixedSize: MaterialStateProperty.all(Size(240.0, 50.0)),
+                    ),
+                  )),
               SizedBox(
                 height: 400,
                 child: GridView(
                   padding: EdgeInsets.all(20.0),
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 5,
-
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 5,
                   ),
                   scrollDirection: Axis.vertical,
                   children: [
                     Card(
-                      semanticContainer: true,
-                        color:Color(0xff92CAE8),
+                        semanticContainer: true,
+                        color: Color(0xff92CAE8),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                            side: BorderSide(color: Color(0xff0077B6),width: 4.0)
-                        ),
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: BorderSide(
+                                color: Color(0xff0077B6), width: 4.0)),
                         elevation: 5,
-                        child:Center(child:Text("My pricings",textAlign:TextAlign.center,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)))),
+                        child: Center(
+                            child: Text("My pricings",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold)))),
                     Card(
-                        color:Color(0xff92CAE8),
+                        color: Color(0xff92CAE8),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                          side: BorderSide(color: Color(0xff0077B6),width: 4.0)
-                        ),
-
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: BorderSide(
+                                color: Color(0xff0077B6), width: 4.0)),
                         elevation: 5,
-                        child:Center(child:Text("Transactions History",textAlign:TextAlign.center,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)))),
+                        child: Center(
+                            child: Text("Transactions History",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold)))),
                     Card(
-                        color:Color(0xff92CAE8),
+                        color: Color(0xff92CAE8),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                            side: BorderSide(color: Color(0xff0077B6),width: 4.0)
-                        ),
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: BorderSide(
+                                color: Color(0xff0077B6), width: 4.0)),
                         elevation: 5,
-                        child:Center(child:Text("My Favorites",textAlign:TextAlign.center,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)))),
+                        child: Center(
+                            child: Text("My Favorites",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold)))),
                     Card(
-                        color:Color(0xff92CAE8),
+                        color: Color(0xff92CAE8),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                            side: BorderSide(color: Color(0xff0077B6),width: 4.0)
-                        ),
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: BorderSide(
+                                color: Color(0xff0077B6), width: 4.0)),
                         elevation: 5,
-                        child:Center(child:Text("My Schedule",textAlign:TextAlign.center,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)))),
+                        child: Center(
+                            child: Text("My Schedule",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold)))),
                   ],
-                ) ,
+                ),
               )
-
             ],
           ),
-
         ),
       ),
-
     );
   }
 }
