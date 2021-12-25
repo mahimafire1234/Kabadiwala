c5ba7b93fa1da9e19cb7bfb554b59f19e8252dcd
KarkiRajiv <rajivkarky15@gmail.com>
2021-12-25 20:20:03 +0545

    :pencil2: fixing typos error

--- lib/pages/base.dart
+++ lib/pages/base.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:login_sprint1/pages/myprofilekabadiwala.dart';
 
 import 'home.dart';
-import 'myprofilekab.dart';
 
 class Base extends StatefulWidget {
   final index;
@@ -17,37 +17,36 @@ class _BaseState extends State<Base> {
 
   _BaseState(this.currentIndex);
 
-  final screens = [
-    Home(),
-    MyProfile()
-  ];
+  final screens = [Home(), CompanyProfile()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Kabadiwala"), iconTheme: IconThemeData(color: Colors.white), elevation: 0.0),
+      appBar: AppBar(
+          title: Text("Kabadiwala"),
+          iconTheme: IconThemeData(color: Colors.white),
+          elevation: 0.0),
       drawer: Drawer(
         child: ListView(
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: [
-             DrawerHeader(
+            DrawerHeader(
               decoration: BoxDecoration(
                 color: Colors.blue,
               ),
-              child: Column(
-                children: [
-                  Icon(CupertinoIcons.profile_circled, size: 100.0),
-                  SizedBox(height: 10.0),
-                  Text("User name", style: TextStyle(color: Colors.white),)
-                ]
-              ),
+              child: Column(children: [
+                Icon(CupertinoIcons.profile_circled, size: 100.0),
+                SizedBox(height: 10.0),
+                Text(
+                  "User name",
+                  style: TextStyle(color: Colors.white),
+                )
+              ]),
             ),
             ListTile(
-              leading: Icon(
-                CupertinoIcons.creditcard,
-                color: Color(0xFF000000)
-              ),
+              leading:
+                  Icon(CupertinoIcons.creditcard, color: Color(0xFF000000)),
               title: const Text('Payment History'),
               onTap: () {
                 // Update the state of the app.
@@ -68,13 +67,10 @@ class _BaseState extends State<Base> {
           ],
         ),
       ),
-      body: IndexedStack(
-          index: currentIndex,
-          children: screens
-      ),
+      body: IndexedStack(index: currentIndex, children: screens),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
-        onTap: (index) => setState(() => currentIndex = index ) ,
+        onTap: (index) => setState(() => currentIndex = index),
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
