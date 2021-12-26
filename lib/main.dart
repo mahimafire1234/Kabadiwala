d27a4f018035e60cc8da360d4c8276530989ed98
Sanjila Shrestha <sanjilashrestha00@gmail.com>
2021-12-26 21:07:55 +0545

    'dropDown'

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: "/priceview",
+      initialRoute: "/insertrate",
       routes: {
         '/signup': (context) => const SignUp(),
         '/login': (context) => LoginPage(),
