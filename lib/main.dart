ee0b8e2c45e6da4a8cea121620ed480e0d7dd80c
rajeesha123 <rajeesha456@gmail.com>
2021-12-29 15:52:02 +0545

    hire items part design completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:login_sprint1/pages/base.dart';
 import 'package:login_sprint1/pages/insertrate.dart';
+import 'package:login_sprint1/pages/items.dart';
 import 'package:login_sprint1/pages/login.dart';
 import 'package:login_sprint1/pages/myprofilekabadiwala.dart';
 import 'package:login_sprint1/pages/oneCompany.dart';
 import 'package:login_sprint1/pages/priceview.dart';
-import 'package:login_sprint1/pages/ratespage.dart';
 import 'package:login_sprint1/pages/set_information.dart';
 import 'package:login_sprint1/pages/signup.dart';
 import 'package:login_sprint1/pages/viewcompany.dart';
@@ -20,19 +20,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: "/viewcompany",
+      initialRoute: "/itemsHire",
       routes: {
         '/signup': (context) => const SignUp(),
         '/login': (context) => LoginPage(),
         '/home': (context) => const Base(index: 0),
         '/viewcompany': (context) => const ViewCompany(),
-        // '/onecompany': (context) => const oneCompany(),
-        // '/insertrate': (context) => const InsertRate(),
-        // '/priceview': (context) => const PriceView(),
+        '/onecompany': (context) => const oneCompany(),
+        '/insertrate': (context) => const InsertRate(),
+        '/priceview': (context) => const PriceView(),
         '/myprofilekabadiwala': (context) => const CompanyProfile(),
         "/setInfo": (context) => SetInformation(),
-        // "/ratespage": (context) => Rates(),
-        "/setInfo": (context) => const Base(index: 0)
+        "/itemsHire": (context) => const ItemsHire()
       },
     );
   }
