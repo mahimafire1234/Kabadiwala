0f41e04038f588278a1bb68c9864676e9903015c
KarkiRajiv <rajivkarky15@gmail.com>
2022-01-01 10:29:18 +0545

    optimizing code

--- lib/services/userservices.dart
+++ lib/services/userservices.dart
@@ -1,37 +1,45 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:login_sprint1/services/Category.dart';
 
-class UserServices{
+class UserServices {
+  static var baseUri = "http://10.0.2.2:5000";
   Future<String?> signup(body) async {
     try {
-      var response = await http
-          .post(Uri.parse("http://10.0.2.2:5000/user/register"),
-          headers: {
-            'Content-type' : 'application/json',
-            "Accept": "application/json",
-          },
-          body: json.encode(body));
+      var response =
+          await http.post(Uri.parse("http://10.0.2.2:5000/user/register"),
+              headers: {
+                'Content-type': 'application/json',
+                "Accept": "application/json",
+              },
+              body: json.encode(body));
       return response.body;
     } catch (e) {
       print(e);
+    }
+  }
+
+  static Future<dynamic> signin(body) async {
+    try {
+      var response = await http.post(Uri.parse("$baseUri/user/login"));
+      return response.body;
+    } on Exception {
+      print("network connection problem");
     }
   }
 
   Future<String?> insertRate(body) async {
     try {
-      var response = await http
-          .post(Uri.parse("http://10.0.2.2:5000/category/insertRate"),
-          headers: {
-            'Content-type' : 'application/json',
-            "Accept": "application/json",
-          },
-          body: json.encode(body));
+      var response =
+          await http.post(Uri.parse("http://10.0.2.2:5000/category/insertRate"),
+              headers: {
+                'Content-type': 'application/json',
+                "Accept": "application/json",
+              },
+              body: json.encode(body));
       return response.body;
     } catch (e) {
       print(e);
     }
   }
-
 }
