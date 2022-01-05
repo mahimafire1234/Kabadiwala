f76ff71e151a7ff0912c1bed22eff8fd1316017f
KarkiRajiv <rajivkarky15@gmail.com>
2022-01-05 22:06:39 +0545

    card

--- lib/pages/booking/booking_request.dart
+++ lib/pages/booking/booking_request.dart
@@ -10,8 +10,9 @@ class BookingRequest extends StatefulWidget {
 }
 
 class _BookingRequestState extends State<BookingRequest> {
-  List<String> itemList = <String>["pending", "accepted", "rejected"];
   String? _startingValue;
+
+  List<String> itemList = <String>["pending", "accepted", "rejected"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,41 +28,41 @@ class _BookingRequestState extends State<BookingRequest> {
                       fontSize: 26.0,
                       color: Color(0xFF0077B6)),
                 ),
-                // Padding(
-                //   padding: EdgeInsets.only(left: 150, top: 20),
-                //   child: Container(
-                //     alignment: AlignmentDirectional.bottomEnd,
-                //     width: 150,
-                //     decoration: BoxDecoration(
-                //         borderRadius: BorderRadius.circular(40.0)),
-                //     child: PhysicalModel(
-                //       borderRadius: BorderRadius.circular(45),
-                //       color: Colors.white,
-                //       shadowColor: const Color(0xff2a2a2a),
-                //       child: DropdownButton(
-                //         alignment: Alignment.center,
-                //         hint: Text("choose request"),
-                //         value: _startingValue,
-                //         icon: const Icon(Icons.arrow_downward),
-                //         elevation: 16,
-                //         onChanged: (String? newValue) {
-                //           setState(() {
-                //             _startingValue = newValue as String;
-                //             print(_startingValue);
-                //           });
-                //         },
-                //         items: itemList.map((String value1) {
-                //           return DropdownMenuItem<String>(
-                //             value: value1,
-                //             child: Text(value1),
-                //           );
-                //         }).toList(),
-                //         isDense: true,
-                //         isExpanded: true,
-                //       ),
-                //     ),
-                //   ),
-                // ),
+                Padding(
+                  padding: EdgeInsets.only(left: 150, top: 20),
+                  child: Container(
+                    alignment: AlignmentDirectional.bottomEnd,
+                    width: 150,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40.0)),
+                    child: PhysicalModel(
+                      borderRadius: BorderRadius.circular(45),
+                      color: Colors.white,
+                      shadowColor: const Color(0xff2a2a2a),
+                      child: DropdownButton(
+                        alignment: Alignment.center,
+                        hint: Text("choose request"),
+                        value: _startingValue,
+                        icon: const Icon(Icons.arrow_downward),
+                        elevation: 16,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _startingValue = newValue as String;
+                            print(_startingValue);
+                          });
+                        },
+                        items: itemList.map((String value1) {
+                          return DropdownMenuItem<String>(
+                            value: value1,
+                            child: Text(value1),
+                          );
+                        }).toList(),
+                        isDense: true,
+                        isExpanded: true,
+                      ),
+                    ),
+                  ),
+                ),
                 SizedBox(
                   height: 30.0,
                 ),
@@ -70,14 +71,19 @@ class _BookingRequestState extends State<BookingRequest> {
                   height: 400,
                   child: ListView.separated(
                     scrollDirection: Axis.vertical,
-                    padding: const EdgeInsets.all(8),
+                    padding: const EdgeInsets.all(20),
                     // shrinkWrap: true,
                     itemCount: itemList.length,
                     itemBuilder: (BuildContext context, int index) {
                       return Container(
-                        height: 50,
-                        color: Colors.amber,
-                        child: Center(child: Text('Entry ${itemList[index]}')),
+                        height: 220,
+                        child: Card(
+                          shadowColor: Colors.black,
+                          semanticContainer: true,
+                          elevation: 2,
+                          margin: EdgeInsets.all(20.0),
+                          child: Text("Order from user"),
+                        ),
                       );
                     },
                     separatorBuilder: (BuildContext context, int index) =>
