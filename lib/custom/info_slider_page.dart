183fdc47f82ed9e62b8ef1b0d7b98f02f6fed090
KarkiRajiv <rajivkarky15@gmail.com>
2022-01-31 21:41:57 +0545

    info page completed

--- lib/custom/info_slider_page.dart
+++ lib/custom/info_slider_page.dart
@@ -35,17 +35,27 @@ class _IntroSliderPageState extends State<IntroSliderPage> {
     );
     slides.add(
       new Slide(
-        title: "Great Discounts",
-        description: "Best discounts on every single service we offer!",
+        title: "Sell Your Papers",
+        description:
+            "We all have our school books .Sell all your books in a tap of your screen",
         pathImage: "assets/images/img_1.png",
       ),
     );
     slides.add(
       new Slide(
-        title: "World Travel",
+        title: "Find Kawadiwalas",
         description:
-            "Book tickets of any transportation and travel the world! ",
+            "Find hundreds of kabadi walas with different selling rates."
+            " Negotiate with them and sell all your unwanted good in one tap",
         pathImage: "assets/images/img_2.png",
+      ),
+    );
+    slides.add(
+      new Slide(
+        title: "Recycle Reuse Reduce",
+        description:
+            " Learn about the three R’s from our blogs written by users. You can also share your ideas and experiences through our blogs",
+        pathImage: "assets/images/img_1.png",
       ),
     );
   }
@@ -118,6 +128,7 @@ class _IntroSliderPageState extends State<IntroSliderPage> {
         Slide currentSlide = slides[i];
         tabs.add(
           Container(
+            // color: Colors.white,
             width: double.infinity,
             height: double.infinity,
             child: Container(
@@ -128,21 +139,24 @@ class _IntroSliderPageState extends State<IntroSliderPage> {
                 children: [
                   Container(
                     padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                    ),
+                    // decoration: BoxDecoration(
+                    //   shape: BoxShape.circle,
+                    //   color: Colors.white,
+                    // ),
                     child: Image.asset(
                       currentSlide.pathImage.toString(),
                       matchTextDirection: true,
-                      height: 60,
+                      height: 150,
                     ),
                   ),
                   Container(
                     margin: EdgeInsets.only(top: 20),
                     child: Text(
                       currentSlide.title.toString(),
-                      style: TextStyle(color: Colors.white, fontSize: 25),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                   Container(
@@ -186,21 +200,24 @@ class _IntroSliderPageState extends State<IntroSliderPage> {
                 children: [
                   Container(
                     padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                    ),
+                    // decoration: BoxDecoration(
+                    //   shape: BoxShape.circle,
+                    //   color: Colors.white,
+                    // ),
                     child: Image.asset(
                       currentSlide.pathImage.toString(),
                       matchTextDirection: true,
-                      height: 60,
+                      height: 150,
                     ),
                   ),
                   Container(
                     margin: EdgeInsets.only(top: 20),
                     child: Text(
                       currentSlide.title.toString(),
-                      style: TextStyle(color: Colors.white, fontSize: 25),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                   Container(
@@ -230,6 +247,91 @@ class _IntroSliderPageState extends State<IntroSliderPage> {
           ),
         );
       }
+      if (i == 3) {
+        Slide currentSlide = slides[i];
+        tabs.add(
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: Container(
+              margin: EdgeInsets.only(bottom: 160, top: 60),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    // decoration: BoxDecoration(
+                    //   shape: BoxShape.circle,
+                    //   color: Colors.white,
+                    // ),
+                    child: Image.asset(
+                      currentSlide.pathImage.toString(),
+                      matchTextDirection: true,
+                      height: 150,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Text(
+                      currentSlide.title.toString(),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30,
+                    ),
+                    child: Text(
+                      currentSlide.description.toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        height: 1.5,
+                      ),
+                      maxLines: 3,
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    margin: EdgeInsets.only(
+                      top: 20,
+                      left: 20,
+                      right: 20,
+                    ),
+                  ),
+                  Container(
+                    height: 105,
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "GET STARTED",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                            EdgeInsets.symmetric(
+                                horizontal: 120.0, vertical: 12.0)),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        )),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      }
     }
     return tabs;
   }
@@ -244,17 +346,17 @@ class _IntroSliderPageState extends State<IntroSliderPage> {
       ),
       renderNextBtn: Text(
         "Next",
-        style: TextStyle(color: Colors.green[700]),
+        style: TextStyle(color: Colors.white, fontSize: 16.0),
       ),
       // renderNextBtn: Visibility(
       //   child: Text("next"),
       //   visible: false,
       // ),
-      showNextBtn: false,
+      // showNextBtn: false,
 
       renderDoneBtn: Text(
         "Done",
-        style: TextStyle(color: Colors.red[700]),
+        style: TextStyle(color: Colors.white, fontSize: 16.0),
       ),
       colorActiveDot: Colors.white,
       sizeDot: 8.0,
